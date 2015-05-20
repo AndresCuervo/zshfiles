6416d6e176e5f82798abf9a1d25940ac3b67a255
Andres Cuervo <andrescuervor@gmail.com>
2015-05-20 10:57:01 -0700

    Initial commit, just adding aliases.zsh

---
+++ aliases.zsh
@@ -0,0 +1,18 @@
+alias ls='ls -AG'
+alias cl='clear && ls'
+alias shme='ssh acuervo@cs.oberlin.edu'
+
+# -----------
+# SSH Aliases
+# -----------
+alias shbook='ssh bookcoop@bookcoop.org'
+alias shweb='ssh tinycarr@cwervo.com'
+alias shed='ssh lucid@ajlc.csr.oberlin.edu'
+alias shclyde='ssh acuervo@clyde.cs.oberlin.edu'
+
+alias dtruss='sudo dtruss'
+alias dtrace='sudo dtrace'
+alias cdvg='cd ~/Documents/webProjects/p5/videoGame && cl'
+
+alias vim-none='vim -u ~/essential.vim -N'
+alias gitrmignored='git ls-files --ignored --exclude-standard | xargs git rm --cached'
