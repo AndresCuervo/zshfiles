ec41c1f922882f1632261281ed6905821e495048
Andres Cuervo <andrescuervor@gmail.com>
2015-06-05 18:16:58 -0400

    New MAMP alias.

--- aliases.zsh
+++ aliases.zsh
@@ -2,6 +2,15 @@ alias rm='rm -i'
 alias ls='ls -AG'
 alias cl='clear && ls'
 alias shme='ssh acuervo@cs.oberlin.edu'
+alias rbenv="nocorrect rbenv"
+alias dtruss='sudo dtruss'
+alias dtrace='sudo dtrace'
+alias cdmamp='cd /Applications/MAMP'
+alias cdvg='cd ~/Documents/webProjects/p5/videoGame && cl'
+alias colorsinfo='sed -n "23,36p" ~/.zsh/lscolors'
+
+alias vim-none='vim -u NONE "+set nocompatible" "+set hls" "+filetype plugin on" "+inoremap jk <Esc>" "+set number" "+set syntax" "+nnoremap fef mx=ggG=\`x" "+set cf"'
+alias gitrmignored='git ls-files --ignored --exclude-standard | xargs git rm --cached'
 
 # -----------
 # SSH Aliases
@@ -10,11 +19,3 @@ alias shbook='ssh bookcoop@bookcoop.org'
 alias shweb='ssh tinycarr@cwervo.com'
 alias shed='ssh lucid@ajlc.csr.oberlin.edu'
 alias shclyde='ssh acuervo@clyde.cs.oberlin.edu'
-
-alias dtruss='sudo dtruss'
-alias dtrace='sudo dtrace'
-alias cdvg='cd ~/Documents/webProjects/p5/videoGame && cl'
-alias colorsinfo='sed -n "23,36p" ~/.zsh/lscolors'
-
-alias vim-none='vim -u NONE "+set nocompatible" "+set hls" "+filetype plugin on" "+inoremap jk <Esc>" "+set number" "+set syntax" "+nnoremap fef mx=ggG=\`x" "+set cf"'
-alias gitrmignored='git ls-files --ignored --exclude-standard | xargs git rm --cached'
