d447fcc04664afaf8c932122a3ea91f3c6cf1f56
Andres Cuervo <andrescuervor@gmail.com>
2015-07-15 23:55:28 -0400

    Update tmux aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,10 +1,11 @@
+alias rm='rm -i'
 alias trash='rmtrash'
 alias ls='ls -AG'
 alias cl='clear && ls'
 alias rbenv="nocorrect rbenv"
 alias dtruss='sudo dtruss'
 alias dtrace='sudo dtrace'
-alias cdcn='cd ~/webProjects/codeNow-Ruby-Curriculum/'
+alias cdcn='cd ~/webProjects/codeNow/'
 alias js='cd ~/webProjects/andrescuervo.github.io/ && bin/jekyll serve'
 alias octo='nocorrect bin/octopress'
 alias cdmamp='cd /Applications/MAMP/htdocs/'
@@ -14,10 +15,22 @@ alias battery='pmset -g ps'
 alias batt='battery'
 alias vim-none='vim -u NONE "+set nocompatible" "+set hls" "+filetype plugin on" "+inoremap jk <Esc>" "+set number" "+set syntax" "+nnoremap fef mx=ggG=\`x" "+set cf"'
 
+# ------------
+# Tmux Aliases
+# ------------
+alias tksess='tmux kill-session -t'
+alias tkpane='tmux kill-pane -t'
+
+# Start up a simple HTTP server with python, put a number after this to specifiy the port, i.e.
+# pyserve 8080
+alias pyserve='python -m SimpleHTTPServer'
+
 # --------------
 # Ruby alias(es)
 # --------------
 alias ri='ri -Tf ansi'
+# Really convenient alias to search through local gems!
+alias gem\?='gem list | ack '
 
 
 # -------------
@@ -31,6 +44,9 @@ alias gshowo='gshow origin'
 # -----------
 # SSH Aliases
 # -----------
+# Set correct terminal for tmux w/ SSH
+alias ssh='TERM=xterm ssh'
+
 alias shbook='ssh bookcoop@bookcoop.org'
 alias shweb='ssh tinycarr@cwervo.com'
 alias shme='ssh acuervo@cs.oberlin.edu'
